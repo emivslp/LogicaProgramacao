f608f5ea06762c5f4f49cbbed5b3ada8162473a7
Emília Santana <emiliavsantana1@gmail.com>
2023-11-03 21:15:30 -0300

    Resolucaodeproblemas
    Aula de Logica de Programação, exercícios do material web

--- resolucao-problemas/Index.js
+++ resolucao-problemas/Index.js
@@ -1,10 +1,43 @@
 // variável, seu valor pode ser alterado no decorrer do programa
-let nomeEstudante = "Helena";
-console.log(nomeEstudante);
+// let nomeEstudante = "Helena";
+// console.log(nomeEstudante);
 
 // constante, seu valor não pode ser alterado
+// const nomeDoEstudante = "Helena";
+// console.log(nomeDoEstudante);
+
+//Em outras linguagens, portanto, você encontrará algo semelhante a:
+// String: nomeEstudante = "Helena";
+
+let nomeEstudante = "Brisa";
+console.log(nomeEstudante);
+
 const nomeDoEstudante = "Helena";
 console.log(nomeDoEstudante);
 
-//Em outras linguagens, portanto, você encontrará algo semelhante a:
-String: nomeEstudante = "Helena";
+// let listaDeEstudantes = ["Brisa","Calado","Mamute"];
+// console.log(listaDeEstudantes);
+// let quantidadeDeEstudantes = listaDeEstudantes.length;
+// console.log(quantidadeDeEstudantes);
+
+// for (let indice = 0; indice < quantidadeDeEstudantes; indice++) {
+//     const alunoCorrente = listaDeEstudantes[indice];
+//     console.log (alunoCorrente);
+// }
+
+// for (let numero = 1; numero <= 10; numero++){
+//     console.log(numero);
+// }
+
+let listaDeEstudantes = ["Brisa", "Calado", "Mamute"];
+let quantidadeDeEstudantes = listaDeEstudantes.length;
+
+listaDeEstudantes.push("Camomila");
+// console.log (listaDeEstudantes);
+
+if (quantidadeDeEstudantes < 5 ) {
+    listaDeEstudantes.push("PenaBranca");
+    console.log(listaDeEstudantes);
+} else {
+    console.log("Não foi possível inserir mais alunos");
+}
